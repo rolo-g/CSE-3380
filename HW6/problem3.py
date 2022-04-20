f870e89c8c3786d604135ccfaf9a3af4943ac393
Rolando Rosales <rolando.rosales@pm.me>
2022-04-20 15:22:00 -0500

    displayed Q and R seperately. final version

--- HW6/problem3.py
+++ HW6/problem3.py
@@ -2,13 +2,13 @@ import numpy as np
 import scipy.linalg as sp
 
 def main():
-    A = ([[1, 0, 4],
-         [-2, 3, -2],
-         [-2, 0, 6]])
+    A = np.array([[1, 0, 4],
+                 [-2, 3, -2],
+                 [-2, 0, 6]])
 
-    Q, r = sp.qr(A)
+    QR = sp.qr(A)
+
+    print("Q = \n", QR[0], "\nR = \n", QR[1])
     
-    print("Q = ", Q)
-
 if __name__ == "__main__":
     main()
